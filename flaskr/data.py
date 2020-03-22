44799b300c1932518937ac73c6ecbe11cb891613
Jeff <jeff@Jeffs-MacBook-Pro.local>
2020-03-22 10:36:52 +0800

    add search input and trends index figure

--- flaskr/data.py
+++ flaskr/data.py
@@ -1,15 +1,22 @@
 from flask import *
 import pandas as pd
 from flaskr import utilities
+import numpy as np
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def show_tables():
-    df_news= utilities.getNewsKeyword('Trump')
-    df_treands = utilities.getGoogleTrends('Trump')
+    search_keywords = request.form.get('keywords','Trump')
+    df_news= utilities.getNewsKeyword(search_keywords)
+    df_treands = utilities.getGoogleTrends(search_keywords)
+    trendsData = np.squeeze(df_treands.values[1:])
+    trendsTime = df_treands.index[1:]
 
     return render_template('view.html',tables=[df_news.to_html(classes='news'), df_treands.to_html(classes='trends')],
+    trendsData =json.dumps(trendsData.tolist()),
+    trendsTime = json.dumps(trendsTime.tolist()),
     titles = ['na', 'Top News', 'Google Trends'])
 
 if __name__ == "__main__":
