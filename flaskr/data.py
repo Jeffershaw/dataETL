6453ed1d8c22de48f27771b4e7055dcd4b2a2de5
meixueyan <xueyanmei429@gmail.com>
2020-03-23 01:23:41 +0000

    TODO schedule

--- flaskr/data.py
+++ flaskr/data.py
@@ -3,23 +3,41 @@ import pandas as pd
 from flaskr import utilities
 import numpy as np
 import json
+from flask_bootstrap import Bootstrap
 import schedule
+import atexit
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route("/",methods=['GET','POST'])
 def show_tables():
-    search_keywords = request.form.get('keywords','Trump')
-    data_news= utilities.getNewsKeyword(search_keywords)
-    df_news = utilities.getSnapshot(data_news)
-    df_treands = utilities.getGoogleTrends(search_keywords)
-    trendsData = np.squeeze(df_treands.values[1:])
-    trendsTime = df_treands.index[1:]
+    search_keywords = request.form.get('keywords','Rate')
+    df_trends = utilities.getGoogleTrends(search_keywords)
+    trends_snapshot = utilities.showSnapshot(df_trends)
+    try:
+        data_news= utilities.getNewsKeyword(search_keywords)
+        df_news = utilities.getSnapshot(data_news)
+        return render_template('view.html',tables=[df_news.to_html(classes='news'), trends_snapshot.to_html(classes='trends')],
+        titles = ['na', 'Top News', 'Google Trends'])
+    except:
+        return redirect(url_for('show_tables'))
 
-    return render_template('view.html',tables=[df_news.to_html(classes='news'), df_treands.to_html(classes='trends')],
-    trendsData =json.dumps(trendsData.tolist()),
-    trendsTime = json.dumps(trendsTime.tolist()),
-    titles = ['na', 'Top News', 'Google Trends'])
+#Another potential way:
+#     except Exception as e:
+#         return str(e)
+
+
+@app.route("/trends",methods=['GET','POST'])
+def show_trends():
+    search_keywords = request.form.get('keywords','Trump')
+    df_trends = utilities.getGoogleTrends(search_keywords)
+    trendsData = np.squeeze(df_trends.values[1:])
+    trendsTime = df_trends.index[1:]
+    return render_template('view.html', trendsData =json.dumps(trendsData.tolist()),
+    trendsTime = json.dumps(trendsTime.tolist()))
 
 if __name__ == "__main__":
+    print(__name__)
     app.run(debug=True)
+    app.run(host='0.0.0.0', port=12345)
